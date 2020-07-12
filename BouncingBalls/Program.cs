2589ea8565ee891f6da38f116eedae474c6e2252
Michal Surmacz <michal.sur96@gmail.com>
2020-07-12 11:45:58 +0200

    Minor refactoring in Bouncing Balls kata.

--- BouncingBalls/Program.cs
+++ BouncingBalls/Program.cs
@@ -7,44 +7,34 @@ namespace BouncingBalls
     {
         static void Main(string[] args)
         {
-            var sw = Stopwatch.StartNew();
-            sw.Start();
-            Console.WriteLine(BouncingBall(100000000000, 0.66, 1.5));
-            sw.Stop();
-            Console.WriteLine($"Executed for: {sw.ElapsedMilliseconds}");
+            Console.WriteLine(BouncingBall(3.0, 0.66, 1.5));
         }
 
-        static int BouncingBall(double h, double bounce, double window)
+        private static int BouncingBall(double h, double bounce, double window)
         {
-            if (IsRequestInvalid(h, bounce, window))
-            {
+            if (!IsRequestValid(h, bounce, window))
                 return -1;
-            }
 
             var result = 0;
             var bounceValue = h;
 
-            while (bounceValue >= window)
+            while (bounceValue > window)
             {
                 result++;
                 bounceValue *= bounce;
 
-                if (bounceValue >= window)
-                {
+                if (bounceValue > window)
                     result++;
-                }
             }
 
             return result;
         }
 
-        private static bool IsRequestInvalid(double h, double bounce, double window)
+        private static bool IsRequestValid(double h, double bounce, double window)
         {
-            return h <= 0 &&
-                   bounce <= 0 &&
-                   bounce >= 1 &&
-                   window >= h;
-
+            return h > 0 &&
+                   (0 < bounce && bounce < 1) &&
+                   window < h;
         }
     }
 }
