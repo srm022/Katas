730597aefe9074a677b90a449569c06339bb6cc7
Michal Surmacz <michal.sur96@gmail.com>
2020-07-13 17:37:20 +0200

    Added Isograms kata.

---
+++ Isograms/Isograms.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Isograms
+{
+    public class Isograms
+    {
+        public static bool IsIsogram(string phrase)
+        {
+            var distinctChars = phrase
+                   .ToLowerInvariant()
+                   .Distinct()
+                   .Count();
+
+            return distinctChars == phrase.Length;
+        }
+    }
+}
---
+++ Isograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using Katas.Utils;
+
+namespace Isograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phrase = "a bc";
+            Kata.Run(() => Isograms.IsIsogram(phrase));
+        }
+    }
+}
